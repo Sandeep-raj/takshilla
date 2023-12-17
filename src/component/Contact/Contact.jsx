63c29ff59dd3c1f73d315235442ab2a86e7d1d75
Sandeep-raj <gurusandeepraj@gmail.com>
2023-12-18 02:14:23 +0530

    conatct page updated

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,9 +1,18 @@
-import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Paper,
+  Tab,
+  Tabs,
+  TextField,
+  Typography,
+} from "@mui/material";
 import "./Contact.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useState } from "react";
-import { Padding } from "@mui/icons-material";
 
 const Contact = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   const [formData, setFormData] = useState({
     fullname: "",
     phonenumber: "",
@@ -38,6 +47,7 @@ const Contact = () => {
       <div className="contact_parallax">
         <Typography variant="h1">Contact Us</Typography>
       </div>
+
       <div style={{ width: "90%", margin: "auto", marginTop: "30px" }}>
         <Paper elevation={4}>
           <div className="contact_enq_form">
@@ -120,10 +130,81 @@ const Contact = () => {
                     Send Enquiry
                   </Button>
                 </Grid>
+
+                <Grid item xs={12} style={{ marginBottom: 16 }} />
               </Grid>
             </form>
           </div>
         </Paper>
+      </div>
+
+      <div className="contact_center">
+        <h1>Our Centers</h1>
+        {/* Add Material UI Vertical Tabs to arrange map and adress of 3 centers */}
+
+        <Tabs
+          variant="scrollable"
+          value={selectedTab}
+          onChange={(event, newValue) => setSelectedTab(newValue)}
+          aria-label="Vertical tabs example"
+          className="vertical-tabs"
+        >
+          <Tab label="Kumhrar" />
+          <Tab label="Sipara" />
+        </Tabs>
+
+        {/* Content for each tab */}
+        {selectedTab === 0 && (
+          <div className="contact_center_detail">
+            {/* Add map and address for Center 1 */}
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={12} md={12} lg={6}>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.091861592869!2d85.11245207497487!3d25.60186357745262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed59c23a5435ef%3A0x9fef2cc5284754ea!2sGoalghar!5e0!3m2!1sen!2sin!4v1702844729885!5m2!1sen!2sin"
+                  style={{ border: "0px" }}
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={6}>
+                <h1>
+                  <LocationOnIcon /> Address
+                </h1>
+                <h3>Takshilla Classes</h3>
+                <p>Eco Pk 1, Rajbansi Nagar</p>
+                <p>Patna, Bihar 800015</p>
+                <p>Contact Number - +91-8220570199</p>
+              </Grid>
+            </Grid>
+          </div>
+        )}
+
+        {selectedTab === 1 && (
+          <div className="contact_center_detail">
+            {/* Add map and address for Center 1 */}
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={12} md={12} lg={6}>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.091861592869!2d85.11245207497487!3d25.60186357745262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed59c23a5435ef%3A0x9fef2cc5284754ea!2sGoalghar!5e0!3m2!1sen!2sin!4v1702844729885!5m2!1sen!2sin"
+                  style={{ border: "0px" }}
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={6}>
+                <h1>
+                  <LocationOnIcon /> Address
+                </h1>
+                <h3>Takshilla Classes</h3>
+                <p>Eco Pk 1, Rajbansi Nagar</p>
+                <p>Patna, Bihar 800015</p>
+                <p>Contact Number - +91-8220570199</p>
+              </Grid>
+            </Grid>
+          </div>
+        )}
       </div>
     </div>
   );
